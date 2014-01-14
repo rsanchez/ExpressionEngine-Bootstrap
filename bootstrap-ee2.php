061d13822a72f9f255b2441d99e3ffd83846c2db
Rob Sanchez <rob@robsanchez.com>
2014-01-14 16:35:20 -0600

    some code cleanup

--- bootstrap-ee2.php
+++ bootstrap-ee2.php
@@ -1,11 +1,10 @@
 <?php
-if (!isset($system_path)) {
-    $system_path = "system";
+if ( ! isset($system_path)) {
+  $system_path = "system";
 }
 
 $assign_to_config['enable_query_strings'] = TRUE;
 $assign_to_config['subclass_prefix'] = 'EE_';
-
 
 if (realpath($system_path) !== FALSE)
 {
@@ -15,20 +14,18 @@ if (realpath($system_path) !== FALSE)
 // ensure there's a trailing slash
 $system_path = rtrim($system_path, '/').'/';
 
-
 define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 define('EXT', '.php');
 define('BASEPATH', str_replace("\\", "/", $system_path.'codeigniter/system/'));
 define('APPPATH', $system_path.'expressionengine/');
 define('FCPATH', str_replace(SELF, '', __FILE__));
 define('SYSDIR', trim(strrchr(trim(str_replace("\\", "/", $system_path), '/'), '/'), '/'));
-define('UTF8_ENABLED', false);
+define('UTF8_ENABLED', FALSE);
 define('CI_VERSION', '2.0');
-//define('AJAX_REQUEST',false);
-define('DEBUG',false);
+define('DEBUG', isset($debug) ? $debug : 0);
 
-require BASEPATH.'core/Common'.EXT;
-require APPPATH.'config/constants'.EXT;
+require BASEPATH.'core/Common.php';
+require APPPATH.'config/constants.php';
 
 $CFG =& load_class('Config', 'core');
 $URI =& load_class('URI', 'core');
@@ -40,24 +37,21 @@ $SEC =& load_class('Security', 'core');
 $loader = load_class('Loader', 'core');
 
 // Load the base controller class
-require BASEPATH.'core/Controller'.EXT;
+require BASEPATH.'core/Controller.php';
 
 function &get_instance()
 {
-    return CI_Controller::get_instance();
+  return CI_Controller::get_instance();
 }
 
 function ee()
 {
-    static $EE;
-    if ( ! $EE) $EE = get_instance();
-    return $EE;
+  static $EE;
+  if ( ! $EE) $EE = get_instance();
+  return $EE;
 }
 
-class EE_Bootstrap_Controller extends CI_Controller {}
+new CI_Controller();
 
-$EE = new EE_Bootstrap_Controller;
-
-$loader->library('core');
-
+ee()->load->library('core');
 ee()->core->bootstrap();
