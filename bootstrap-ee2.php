46ec2a31632d42f0ec33c1afda70e84d6b510d1d
Rob Sanchez <rob@robsanchez.com>
2014-02-14 12:22:33 -0600

    Enable $assign_to_config compat, fix missing Utf8 class

--- bootstrap-ee2.php
+++ bootstrap-ee2.php
@@ -1,4 +1,7 @@
 <?php
+
+global $assign_to_config, $system_path, $debug, $CFG, $URI, $IN, $OUT, $LANG, $SEC, $loader;
+
 if ( ! isset($system_path)) {
   $system_path = "system";
 }
@@ -6,9 +9,8 @@ if ( ! isset($system_path)) {
 $assign_to_config['enable_query_strings'] = TRUE;
 $assign_to_config['subclass_prefix'] = 'EE_';
 
-if (realpath($system_path) !== FALSE)
-{
-	$system_path = realpath($system_path).'/';
+if (realpath($system_path) !== FALSE) {
+  $system_path = realpath($system_path).'/';
 }
 
 // ensure there's a trailing slash
@@ -20,7 +22,6 @@ define('BASEPATH', str_replace("\\", "/", $system_path.'codeigniter/system/'));
 define('APPPATH', $system_path.'expressionengine/');
 define('FCPATH', str_replace(SELF, '', __FILE__));
 define('SYSDIR', trim(strrchr(trim(str_replace("\\", "/", $system_path), '/'), '/'), '/'));
-define('UTF8_ENABLED', FALSE);
 define('CI_VERSION', '2.0');
 define('DEBUG', isset($debug) ? $debug : 0);
 
@@ -28,6 +29,10 @@ require BASEPATH.'core/Common.php';
 require APPPATH.'config/constants.php';
 
 $CFG =& load_class('Config', 'core');
+if (isset($assign_to_config)) {
+	$CFG->_assign_to_config($assign_to_config);
+}
+$UNI =& load_class('Utf8', 'core');
 $URI =& load_class('URI', 'core');
 $IN	=& load_class('Input', 'core');	
 $OUT =& load_class('Output', 'core');
